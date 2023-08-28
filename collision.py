a184dfd32404456d60567500c07c34d3994c8c39
TrunGodword <godj59@yahoo.com>
2023-08-28 15:23:16 +0800

    Updated jumping + collision bars(not working)

--- collision.py
+++ collision.py
@@ -3,9 +3,51 @@ import settings
 
 screen = settings.pygame.display.get_surface()
 
-width, height = screen.get_width(), screen.get_height()
+maxWidth, maxHeight = screen.get_width(), screen.get_height()
 
-width /= 8
-height /= 8
+width = maxWidth // 8
+height = maxHeight// 8
+array1= []
+array2= []
+array3= []
+array4= []
+array5= []
+array6= []
+array7= []
+array8= []
+
+line1 = []
+line2 = []
+line3 = []
+line4 = []
+line5 = []
+line6 = []
+line7 = []
+line8 = []
+lines = [line1, line2, line3, line4, line5, line6, line7, line8]
+
+array1.append([0, 0])
+array2.append([width, 0])
+array3.append([width*2, 0])
+array4.append([width*3, 0])
+array5.append([width*4, 0])
+array6.append([width*5, 0])
+array7.append([width*6, 0])
+array8.append([width*7, 0])
+arrays= [array1, array2, array3, array4, array5, array6, array7, array8]
+
 for value in range(1, 9):
-    print(value)
+    value2 = value - 1
+    array1.append([array1[value2][0], array1[value2][1]+height])
+    array2.append([array2[value2][0], array2[value2][1]+height])
+    array3.append([array3[value2][0], array3[value2][1]+height])
+    array4.append([array4[value2][0], array4[value2][1]+height])
+    array5.append([array5[value2][0], array5[value2][1]+height])
+    array6.append([array6[value2][0], array6[value2][1]+height])
+    array7.append([array7[value2][0], array7[value2][1]+height])
+    array8.append([array8[value2][0], array8[value2][1]+height])
+
+# for value in range(0, len(arrays)):
+    # print(arrays[value])
+
+surface = pygame.Surface((width, height))
--- player.py
+++ player.py
@@ -1,65 +1,53 @@
-import pygame
+import sys, pygame
 
 class Player(pygame.sprite.Sprite):
-    # def __init__ буюу Class хуваарьлагдахад автоматаар уншигдах функц:
-    # Хамгийн эхний (self) гэж буй нь өөрт хуваарьлагдаж буй бүх өгөгдлийг өөр дээрээ буулгаж өгөх нэршил юм.
-    def __init__(self, x, y, up, down, left, right, color, health, attackA ,sprite_sheet, animation_steps):
+    def __init__(self, x, y, up, down, left, right, color, health, attackA):
         super().__init__()
-        # Player character-ийн урт ба өргөн.
         self.image = pygame.Surface((50, 70))
-        # Player character-ийн өнгө.
         self.color = (color)
         self.image.fill((color))
-        # Өнгө ба урт, өргөний хэмжээг өгсөний дараа хэмжээний өгөгдлүүдийг өөр дээрээ self.rect гэж авах
         self.rect = self.image.get_rect()
-        # Self.rect-ийн x ба y хэмжээг мөн өөр дээрээ авах.
         self.rect.x = x
         self.rect.y = y
-        # Player character-ийн хурд ба амь.
         self.speed = 10
         self.health = health
-        self.mass = 1
+        self.mass = 20
         self.gravity = 10
         self.jumping = False
-        self.animation_list = self.load_images(sprite_sheet, animation_steps)
+        self.jumpcd = 40
+        self.initialmass = self.mass
+        self.dt = round(1/60, 4)
         
-        # up, down, left, right-ийн оронд бид гар keyboard-ний товчлуурууд хуваарьлах бөгөөд self.up/down/left/right дээр тусгаж авч буй нь:
         self.up =  up
         self.down = down
         self.left = left
         self.right = right
         self.attackA = attackA
     
-    
-    # Update хэсэг буюу Player character-ийн EVENT LOOP:
     def update(self):
-        # Энэ нь Товчлуур дарагдаж буйг шалгадаг функцийг өөр дээрээ хадгалж өгч байгаа юм.
         keys = pygame.key.get_pressed()
+
         
-        
-        if self.jumping:
-            self.mass -= 10
-            self.jumping = False
-        if self.mass < 1:
+        if self.mass < self.initialmass:
             self.mass += 1
-                                        # Force 
-        self.rect.y = self.rect.y + (self.mass * self.gravity)
 
+        if self.jumping == True and self.jumpcd >= 1:
+            self.jumpcd -= 1
+        else:
+            self.jumping = False
+            self.jumpcd = 40
+                
+        
+        self.rect.y += self.mass + (self.gravity * self.dt)
 
-        # Дараа нь, left, right, up, down гэдэг дээр хуваарьлагдсан товчнууд дарагдаж байвал 
-        # Player character-ийн x эсвэл y өгөгдлийг өөрийнх нь speed өгөгдлөөр хасч эсвэл нэмж өгч буй нь
         if keys[self.left]:
             self.rect.x -= self.speed
         if keys[self.right]:
             self.rect.x += self.speed
-        
-        # if keys[self.down]:
-            # self.rect.y += self.speed
+        if keys[self.up] and self.jumping == False:
+            self.mass = -(self.mass)
+            self.jumping = True
 
-
-        # Дэлгэцнээс гардаггүй болох хэсэг.
-        # Хэрвээ Player character-ийн x эсвэл y дэлгэцний өөрийн урт эсвэл өргөнөөс хэтэрвэл,
-        # эсвэл 0 буюу дэлгэцний хүрээнээс хэтэрвэл x ба y өгөгдлүүдийг тэнд нь тэнцүүлж буюу гацсан мэт еффектийг оруулах.
         if self.rect.left < 0:
             self.rect.left = 0
         elif self.rect.right > 640:
